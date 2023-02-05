6ea75782c0a8845d822198b074f77faaec07b992
lucasalexsandro <devlucasalexsandro@gmail.com>
2023-02-05 15:33:15 -0300

    Bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import path from 'path'
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'src/index.html'),
+        about: path.resolve(__dirname, 'src/about.html')
+      }
+    }
+  }
+}
+
